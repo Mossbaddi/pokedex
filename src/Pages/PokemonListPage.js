96a80222adc0157502d9d1255d69247454ee12e8
Mostapha <mossb.addi@gmail.com>
2024-03-13 15:12:18 +0100

    Récupération de tous les pokemon, sans les détails ni pagination

--- src/Pages/PokemonListPage.js
+++ src/Pages/PokemonListPage.js
@@ -1,16 +1,32 @@
 // src/pages/PokemonListPage.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonCard from '../Components/PokemonCard/PokemonCard';
+import axios from 'axios';
+
 
 const PokemonListPage = () => {
-  return (
-    <div>
-      <h1>Liste des Pokémon</h1>
-      <PokemonCard name="Pikachu" imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" types={["electric"]} />
-      <PokemonCard name="Charizard" imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" types={["fire", "flying"]} />
-      {/* Plus de cartes Pokémon peuvent être ajoutées ici */}
-    </div>
-  );
-};
-
-export default PokemonListPage;
+    const [pokemons, setPokemons] = useState([]);
+  
+    useEffect(() => {
+      const fetchPokemons = async () => {
+        const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+        setPokemons(res.data.results);
+      };
+  
+      fetchPokemons();
+    }, []);
+  
+    return (
+      <div>
+        <h1>Liste des Pokémon</h1>
+        <div>
+          {pokemons.map(pokemon => (
+            <PokemonCard key={pokemon.name} name={pokemon.name} imageUrl={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`} types={[]} />
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default PokemonListPage;
+  
