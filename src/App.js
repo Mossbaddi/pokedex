a16686475645a33b9328c9cb101ed950b9764cdb
Mostapha <mossb.addi@gmail.com>
2024-03-13 14:20:22 +0100

    Modification de App.js pour utiliser la navbar/le router.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import PokemonCard from "./Components/PokemonCard/PokemonCard";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      {/* Pokemon card Pikachu*/}
-      <PokemonCard name="Pikachu" imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" types={["electric"]}></PokemonCard>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<div>Accueil</div>} />
+      {/* Le composant PokemonCard a été déplacé ici parce que dans une application React utilisant les routes,
+       on doit forcément afficher ces composants dans une des routes.
+       Dans ce cas, les composants sont affichés lorsque l'on accède à la route "/pokemon-list".
+        */}
+      <Route path="/pokemon-list" element={<div>
+           <PokemonCard name="Pikachu" imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" types={["electric"]}></PokemonCard>
+          <PokemonCard name="Charizard" imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" types={["fire", "flying"]}></PokemonCard>
+        </div>} />
+      <Route path="/my-pokedex" element={<div>Mon Pokédex</div>} />
+    </Routes>
+  </BrowserRouter>
 
-      {/* Pokemon card Charizard */}
-      <PokemonCard name="Charizard" imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" types={["fire", "flying"]}></PokemonCard>
-    </div>
+
   );
 }
 
